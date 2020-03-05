bc9f5f1429cd55e98109978fb1983b9ebcbd8f46
Saurabh S <saurabhs@mailbox.org>
2020-03-05 18:34:24 +0530

    Fix clock to have extended/contracted time at random intervals

--- Assets/Scripts/Core/Clock.cs
+++ Assets/Scripts/Core/Clock.cs
@@ -12,25 +12,37 @@ namespace EvoSim.Core
 
     public class Clock : MonoBehaviour
     {
-        [SerializeField] private float _extendedTime = 0.7f;
+        [SerializeField] private float _extendedTimeDuration = 0.7f;
+
+        [SerializeField] private float _tickLimit = 1f;
+
+        private Vector2 _extendedTimeRange = new Vector2(0, 0.7f);
 
         [SerializeField] private EState _state = EState.None;
 
-        private float _contractedTime = 0.3f;
+        private float _tick = 0f;
 
-        private float _time = 0f;
+        private float _circularTickMax = 0f;
 
         public EState State => _state;
 
-        private void Start() => _contractedTime = 1 - _extendedTime;
+        private void Start()
+        {
+            _extendedTimeRange.x = Random.Range(0, _tickLimit - 0.1f);
+            _extendedTimeRange.y = _extendedTimeRange.x + _extendedTimeDuration;
+            _circularTickMax = _extendedTimeRange.y;
+
+            if(_extendedTimeRange.y > _tickLimit)
+                _extendedTimeRange.y -= _tickLimit;
+        }
 
         private void Update()
         {
-            _time += Time.deltaTime;
-            if(_time > 1)
-                _time = 0f;
+            _tick += Time.deltaTime;
+            if(_tick > _tickLimit)
+                _tick = 0f;
 
-            _state = _time < _extendedTime ? EState.Extended : EState.Contracted;
+            _state = (_tick > _extendedTimeRange.x && _tick < _circularTickMax) ? EState.Extended : EState.Contracted;
         }
     }
 }
